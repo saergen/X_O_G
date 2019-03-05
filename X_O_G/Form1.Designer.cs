05be76b651a4f2fd09248d61e71f42476c8213ab
saergen <43776931+saergen@users.noreply.github.com>
2019-03-05 11:54:50 +0300

    Initial commit

---
+++ X_O_G/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace X_O_G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leaveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.heplToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A1.Location = new System.Drawing.Point(16, 58);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A2.Location = new System.Drawing.Point(122, 58);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 0;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B1.Location = new System.Drawing.Point(16, 164);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 0;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A3.Location = new System.Drawing.Point(228, 58);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 0;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B2.Location = new System.Drawing.Point(122, 164);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 0;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B3.Location = new System.Drawing.Point(228, 164);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 0;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.MouseCaptureChanged += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C1.Location = new System.Drawing.Point(16, 270);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 0;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.MouseCaptureChanged += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C2.Location = new System.Drawing.Point(122, 270);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 0;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.MouseCaptureChanged += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C3.Location = new System.Drawing.Point(228, 270);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 0;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.MouseCaptureChanged += new System.EventHandler(this.button_click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.BackgroundImage = global::X_O_G.Properties.Resources.Regal_Caracal_Amonkhet_MtG_Art;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.heplToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MaximumSize = new System.Drawing.Size(600, 450);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(554, 55);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.leaveGameToolStripMenuItem});
+            this.gameToolStripMenuItem.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gameToolStripMenuItem.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(100, 51);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(243, 48);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // leaveGameToolStripMenuItem
+            // 
+            this.leaveGameToolStripMenuItem.Name = "leaveGameToolStripMenuItem";
+            this.leaveGameToolStripMenuItem.Size = new System.Drawing.Size(243, 48);
+            this.leaveGameToolStripMenuItem.Text = "Leave Game";
+            this.leaveGameToolStripMenuItem.Click += new System.EventHandler(this.leaveGameToolStripMenuItem_Click);
+            // 
+            // heplToolStripMenuItem
+            // 
+            this.heplToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.heplToolStripMenuItem.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.heplToolStripMenuItem.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.heplToolStripMenuItem.Name = "heplToolStripMenuItem";
+            this.heplToolStripMenuItem.Size = new System.Drawing.Size(93, 51);
+            this.heplToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(181, 52);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::X_O_G.Properties.Resources.Regal_Caracal_Amonkhet_MtG_Art;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(554, 381);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(570, 420);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(570, 420);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X/O_Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem heplToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leaveGameToolStripMenuItem;
+    }
+}
+
---
+++ X_O_G/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O_G
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Regal_Caracal","XOG_About");
+        }
+
+        private void leaveGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            foreach (Control c in Controls)
+            {
+                bool winner = false;
+
+                if ((A1.Text == A2.Text) && (A2.Text == A3.Text)&&(!A1.Enabled))
+                    winner = true;
+                else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                    winner = true;
+                else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                    winner = true;
+                else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                    winner = true;
+                else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                    winner = true;
+                else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                    winner = true;
+                else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                    winner = true;
+                else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+                    winner = true;               
+
+
+                if (winner)
+                {
+                    disableButtons();
+                    String the_winner = "";
+                    if (turn)
+                        the_winner = "O";
+                    else
+                        the_winner = "X";
+                    MessageBox.Show(the_winner + "'s Victory!", "Game Result:");
+                }
+
+                else
+                {
+                    if(turn_count==9)
+                        MessageBox.Show("Nobody's Victory!");
+                }
+
+            }
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+
+                }
+            }
+            catch { }
+
+
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
